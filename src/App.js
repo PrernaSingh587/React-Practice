c8da2fcb4c49fc06b6bb3ef2317953e9cae29ec8
Prerna <psingh1999587@gmail.com>
2021-05-09 17:00:55 +0530

    hh

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import Expense from './components/ExpenseItem.js'
+import ExpenseItem from './components/Expenses/ExpenseItem';
+import Expenses from './components/Expenses/Expenses';
 function App() {
-  const expenseDate = new Date(2021,2,28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   return (
-    <div className="App">
-      <Expense amt={expenseAmount} date={expenseDate} title={expenseTitle}/>
-      <Expense amt={expenseAmount} date={expenseDate} title={expenseTitle}/>
-      <Expense amt={expenseAmount} date={expenseDate} title={expenseTitle}/>
+    <div>
+      <Expenses items={expenses}/>
     </div>
   );
 }
